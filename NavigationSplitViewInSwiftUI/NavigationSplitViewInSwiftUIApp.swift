17b74604f0b1527ea510c8bd5947a15319d4ef8a
Ramill <ramill_ibragimov@icloud.com>
2022-10-02 09:09:51 +0300

    Initial Commit

---
+++ NavigationSplitViewInSwiftUI/NavigationSplitViewInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSplitViewInSwiftUIApp.swift
+//  NavigationSplitViewInSwiftUI
+//
+//  Created by Ramill Ibragimov on 02.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSplitViewInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
